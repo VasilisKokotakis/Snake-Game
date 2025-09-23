67c793effd58413c1748e680ad3c6f0f79529381
vasilis_kokotakis <vasilis@Vasilis.home>
2025-09-23 17:52:52 +0200

    Refactored snake game

--- snake.py
+++ snake.py
@@ -1,31 +1,38 @@
 from turtle import Turtle
-
-STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-UP    = 90
-DOWN  = 270
-LEFT  = 180
+UP = 90
+DOWN = 270
+LEFT = 180
 RIGHT = 0
 
 class Snake:
+
     def __init__(self):
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
 
     def create_snake(self):
-        for position in STARTING_POSITION:
+        for position in STARTING_POSITIONS:
             self.add_segment(position)
 
     def add_segment(self, position):
-        new_segment = Turtle(shape="square")
+        new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
     def extend(self):
-        # add a new segment to the snake
         self.add_segment(self.segments[-1].position())
 
     def move(self):
